9c99c2a6ebc4060aacfc005b67ee81a9dd4ada30
theor-unity <theor@unity3d.com>
2017-07-29 00:23:18 -0400

    zoom, draw voronoi

--- Mapgen/MainWindow.xaml.cs
+++ Mapgen/MainWindow.xaml.cs
@@ -44,12 +44,47 @@ namespace Mapgen
             Title += string.Format(" ({0} points)", _vm.NumberOfVertices);
 
             skElement.PaintSurface += SkElementOnPaintSurface;
+
+            this.MouseDown += (sender, args) => skElement.InvalidateVisual();
+
+            this.MouseWheel += (_, a) =>
+            {
+                scale = Clamp(scale * (a.Delta > 0 ? 1.05f : 0.95f), 0.1f, 100);
+                RefreshMatrix();
+            };
+        }
+
+        private void RefreshMatrix()
+        {
+            matrix.SetScaleTranslate(scale, scale, 0, 0);
+            skElement.InvalidateVisual();
+        }
+
+        public float scale;
+        private SKMatrix matrix = SKMatrix.MakeIdentity();
+
+        static float Clamp(float v, float a, float b)
+        {
+            return v < a ? a : (v > b ? b : v);
         }
 
         private void SkElementOnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
-            using(var p = new SKPaint{Color = SKColors.Red})
-            args.Surface.Canvas.DrawCircle(args.Info.Width / 2, args.Info.Height / 2,Math.Min(args.Info.Width / 2, args.Info.Height / 2), p);
+            (float w, float h) = (args.Info.Width, args.Info.Height);
+            var c = args.Surface.Canvas;
+            c.Clear(SKColors.Black);
+            //using (var p = new SKPaint { Color = SKColors.Red })
+            //    c.DrawCircle(w / 2, h / 2, Math.Min(w / 2, h / 2), p);
+            var f = 1.0 / Math.Min(w / 2, h / 2);
+            c.SetMatrix(matrix);
+            using (var p = new SKPaint { Color = SKColors.DarkBlue, IsStroke = true, StrokeWidth = 0.7f, StrokeCap = SKStrokeCap.Round })
+            {
+                c.DrawPoints(SKPointMode.Polygon, delaunayvertices, p);
+            }
+            using (var p = new SKPaint { Color = SKColors.Cyan, IsStroke = true, StrokeWidth = 2, StrokeCap = SKStrokeCap.Round})
+            {
+                c.DrawPoints(SKPointMode.Points, _vm.SkVertices, p);
+            }
         }
 
         private void Create(List<Vertex> vertices)
@@ -68,7 +103,8 @@ namespace Mapgen
             }
             txtBlkTimer.Text = string.Format("{0} faces", _vm.VoronoiMesh.Vertices.Count());
 
-            _vm.Vertices = vertices;
+            _vm.vertices = vertices;
+            
             //_vm.ShowVertices(drawingCanvas.Children);
 
             btnFindDelaunay.IsEnabled = true;
@@ -78,16 +114,16 @@ namespace Mapgen
         private void btnMakePoints_Click(object sender, RoutedEventArgs e)
         {
             var vs = new List<Vertex>();
-            _vm.MakeRandom(vs, new Size(drawingCanvas.ActualWidth,drawingCanvas.ActualHeight));
+            _vm.MakeRandom(vs, new Size(skElement.CanvasSize.Width, skElement.CanvasSize.Height));
             Create(vs);
         }
 
-        public Canvas drawingCanvas { get; private set; }
+        //public Canvas drawingCanvas { get; private set; }
 
-
+        private SKPoint[] delaunayvertices = new SKPoint[0];
         private void btnFindDelaunay_Click(object sender, RoutedEventArgs e)
         {
-            drawingCanvas.Children.Clear();
+            //drawingCanvas.Children.Clear();
 
             btnFindDelaunay.IsEnabled = false;
             btnFindVoronoi.IsEnabled = true;
@@ -99,9 +135,12 @@ namespace Mapgen
 
             //double min = 1;
             //double max = 0;
-            foreach (var cell in _vm.VoronoiMesh.Vertices)
+            delaunayvertices = new SKPoint[_vm.VoronoiMesh.Vertices.Count() * 3];
+            for (var index = 0; index < _vm.VoronoiMesh.Vertices.ToList().Count; index++)
             {
+                var cell = _vm.VoronoiMesh.Vertices.ToList()[index];
                 double value = (p.GetValue(cell.Centroid.X, cell.Centroid.Y, 0) + 1) / 2.0;
+
                 //if (value < min)
                 //{
                 //    min = value;
@@ -116,37 +155,47 @@ namespace Mapgen
                     value = 0;
                 if (value > 1)
                     value = 1;
-                byte b = (byte) (value * 255); 
-                cell.Brush = new SolidColorBrush(Color.FromRgb(b,b,b));
-                drawingCanvas.Children.Add(cell.Visual);
+                byte b = (byte)(value * 255);
+                cell.Brush = new SolidColorBrush(Color.FromRgb(b, b, b));
+                delaunayvertices[index * 3] = cell.Vertices[0].ToSkPoint();
+                delaunayvertices[index * 3 + 1] = cell.Vertices[1].ToSkPoint();
+                delaunayvertices[index * 3 + 2] = cell.Vertices[2].ToSkPoint();
+
+                //drawingCanvas.Children.Add(cell.Visual);
             }
 
-            _vm.ShowVertices(drawingCanvas.Children);
+            //_vm.ShowVertices(drawingCanvas.Children);
 
             foreach (var cell in _vm.VoronoiMesh.Vertices)
             {
-                drawingCanvas.Children.Add(new Vertex(cell.Centroid.X, cell.Centroid.Y, Brushes.Cyan));
+                //drawingCanvas.Children.Add(new Vertex(cell.Centroid.X, cell.Centroid.Y, Brushes.Cyan));
             }
 
         }
 
         private void btnFindVoronoi_Click(object sender, RoutedEventArgs e)
         {
-            drawingCanvas.Children.Clear();
+            //drawingCanvas.Children.Clear();
             btnFindVoronoi.IsEnabled = false;
             btnFindDelaunay.IsEnabled = true;
 
-            var drawingCanvasChildren = drawingCanvas.Children;
-            _vm.ShowVoronoi(drawingCanvasChildren);
+            //var drawingCanvasChildren = drawingCanvas.Children;
+            //_vm.ShowVoronoi(drawingCanvasChildren);
 
-            _vm.ShowVertices(drawingCanvas.Children);
-            drawingCanvas.Children.Add(new Rectangle { Width = drawingCanvas.ActualWidth, Height = drawingCanvas.ActualHeight, Stroke = Brushes.Black, StrokeThickness = 3 });
+            //_vm.ShowVertices(drawingCanvas.Children);
+            //drawingCanvas.Children.Add(new Rectangle { Width = drawingCanvas.ActualWidth, Height = drawingCanvas.ActualHeight, Stroke = Brushes.Black, StrokeThickness = 3 });
         }
     }
 
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-        public List<Vertex> Vertices;
+        public List<Vertex> vertices
+        {
+            get { return _vertices; }
+            set { _vertices = value;
+                SkVertices = value.Select(v => new SKPoint((float)v.Position[0], (float)v.Position[1])).ToArray();
+            }
+        }
 
         private int _numberOfVertices = 1500;
         public int NumberOfVertices
@@ -164,6 +213,8 @@ namespace Mapgen
         private double _freq = 1;
 
         public VoronoiMesh<Vertex, Cell, VoronoiEdge<Vertex, Cell>> VoronoiMesh;
+        public SKPoint[] SkVertices = new SKPoint[0];
+        private List<Vertex> _vertices;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -215,21 +266,29 @@ namespace Mapgen
 
         public void ShowVertices(UIElementCollection drawingCanvasChildren)
         {
-            for (var i = 0; i < Vertices.Count; i++)
+            for (var i = 0; i < vertices.Count; i++)
             {
-                drawingCanvasChildren.Add(Vertices[i]);
+                drawingCanvasChildren.Add(vertices[i]);
             }
         }
 
         public void MakeRandom(List<Vertex> vertices, Size size)
         {
             var r = new Random(42);
-            
+
             for (var i = 0; i < NumberOfVertices; i++)
             {
                 var vi = new Vertex(size.Width * r.NextDouble(), size.Height * r.NextDouble());
                 vertices.Add(vi);
             }
+        }
+    }
+
+    static class Ext
+    {
+        public static SKPoint ToSkPoint(this MIConvexHull.IVertex v)
+        {
+            return new SKPoint((float)v.Position[0], (float)v.Position[1]);
         }
     }
 }
